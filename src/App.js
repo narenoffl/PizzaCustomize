05189bfa0460a2c24e008ae82c4afe671d98ea8b
Naren Purushothaman <naren.p.2018.cse@ritchennai.edu.in>
2020-09-21 18:17:50 +0530

    pizzareactapp

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Customize from "./components/Customize";
+import Checkout from "./components/Checkout";
+
+function App() {
+  const [ingredients, setIngredients] = useState({
+    basil: false,
+    cheese: false,
+    mushroom: false,
+    olive: false,
+    pineapple: false,
+    tomato: false,
+  });
+
+  useEffect(() => {
+    const data = localStorage.getItem("ingredients");
+    if (data) {
+      setIngredients(JSON.parse(data));
+    }
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Customize
+              ingredients={ingredients}
+              setIngredients={setIngredients}
+            />
+          </Route>
+          <Route path="/checkout">
+            <Checkout ingredients={ingredients} />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
